bd3012856b01ff8303831f677dcaa576962534d3
fiocassina <fiorellacassina@gmail.com>
2026-02-16 15:43:56 -0300

    configuracion smtp de gmail para railway

--- src/config/mailer.ts
+++ src/config/mailer.ts
@@ -13,7 +13,9 @@ if (!emailUser || !emailPass) {
 }
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true, 
   auth: {
     user: emailUser,
     pass: emailPass,
--- src/utils/validaciones.ts
+++ src/utils/validaciones.ts
@@ -1,19 +1,17 @@
 export const esFechaFutura = (fecha: Date): boolean => {
     const hoy = new Date();
-    // "Reseteamos" la hora de hoy a las 00:00:00
-    // Así, si la fecha es "hoy a la tarde", va a ser mayor a "hoy a las 00:00" y pasará.
+    
     hoy.setHours(0, 0, 0, 0);
 
     return fecha.getTime() >= hoy.getTime();
 };
 export const esEmailValido = (email: string): boolean => {
-    // Regex simple para validar formato de email
+
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
 };
 
 export const esNotaValida = (nota: number): boolean => {
-    // La nota debe ser mayor o igual a 1 y menor o igual a 10
     return nota >= 1 && nota <= 10;
 };
 
