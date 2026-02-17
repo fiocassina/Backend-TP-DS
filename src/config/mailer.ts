1c04bcaea1f96831a9e749e918883113dd70d226
Candela Garcia <candegarciamo@gmail.com>
2026-02-17 10:38:54 -0600

    conexion mail

--- src/config/mailer.ts
+++ src/config/mailer.ts
@@ -2,35 +2,35 @@ import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
 import path from 'path';
 
+// Configuración de variables de entorno 
 dotenv.config({ path: path.resolve(process.cwd(), 'variables.env') });
 
 const emailUser = process.env.EMAIL_USER;
 const emailPass = process.env.EMAIL_PASS;
-/*
+
 if (!emailUser || !emailPass) {
-  console.error('Faltan las variables EMAIL_USER o EMAIL_PASS en el archivo variables.env');
-  process.exit(1); 
+  console.warn(' ADVERTENCIA: Faltan las variables de entorno del correo (EMAIL_USER o EMAIL_PASS). Los mails no saldrán.');
 }
-*/
+
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
+  port: 587,            
+  secure: false,        
   auth: {
-    user: emailUser || '',
+    user: emailUser || '', 
     pass: emailPass || '',
   },
-});
-
-// Verificamos la conexión
-/*transporter.verify((error, success) => {
-  if (error) {
-    console.error('Error al conectar con el servicio de correos:', error);
-  } else {
-    if (process.env.NODE_ENV !== 'test') {
-        console.log('El servicio de correos está listo y conectado para enviar mensajes.');
-    }
+  tls: {
+    rejectUnauthorized: false 
   }
 });
-*/
+
+transporter.verify()
+  .then(() => {
+    console.log('El servicio de correos está CONECTADO y listo.');
+  })
+  .catch((error) => {
+    console.error('Error al conectar con Gmail:', error);
+  });
+
 export default transporter;
